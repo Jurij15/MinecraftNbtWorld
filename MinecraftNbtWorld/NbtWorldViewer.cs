6b17919f774a4e9d60faff83a7ff0c6d0639392d
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2023-08-24 17:26:00 +0200

    add versioning

---
+++ MinecraftNbtWorld/NbtWorldViewer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftNbtWorldViewer
+{
+    public class NbtWorldViewer
+    {
+        //versioning: [version][type/branch][dateofrelease]
+        public static readonly string Version = "1.0-Development24.8.2023";
+    }
+}
